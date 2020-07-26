2226cd8af36d6e0b64cb68d57c570d7a63f3e9f1
osmmyk <tavjzr3@gmail.com>
2020-07-27 00:17:00 +0900

    “swipe”

--- animal_picture_book_app/lib/main.dart
+++ animal_picture_book_app/lib/main.dart
@@ -39,24 +39,21 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('APP'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('images/logo.png'),
-            FlatButton(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.redAccent,
-              onPressed: () => {
-                Navigator.pushNamed(context, '/animalpage')},
-              child: new Text("はじめる"),
-            ),
-          ],
-        )
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/logo.png'),
+          FlatButton(
+            padding: EdgeInsets.all(10.0),
+            color: Colors.redAccent,
+            onPressed: () => {Navigator.pushNamed(context, '/animalpage')},
+            child: new Text("はじめる"),
+          ),
+        ],
+      )),
     );
   }
 }
-
 
 class AnimalPage extends StatelessWidget {
   @override
@@ -65,27 +62,95 @@ class AnimalPage extends StatelessWidget {
       appBar: new AppBar(
         title: const Text('animal'),
       ),
-      body: new Stack(
-          children: <Widget>[
-            new Container(
-              decoration: new BoxDecoration(
-                image: new DecorationImage(
-                  image: new AssetImage(
-                      "images/bg_animal.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
+      body: new Stack(children: <Widget>[
+        new AnimalPageView(),
+        new Center(
+          child: new FlatButton(
+            padding: EdgeInsets.all(10.0),
+            color: Colors.redAccent,
+            onPressed: () => {Navigator.pop(context)},
+            child: new Text("もどる"),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class AnimalPageView extends StatefulWidget {
+  @override
+  _AnimalPageViewState createState() => _AnimalPageViewState();
+}
+
+class _AnimalPageViewState extends State<AnimalPageView> {
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      children: [
+        AnimalPage1Widget(),
+        AnimalPage2Widget(),
+        AnimalPage3Widget(),
+      ],
+    );
+  }
+}
+
+class AnimalPage1Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(children: <Widget>[
+        new Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("images/animalpage1_bg.png"),
+              fit: BoxFit.cover,
             ),
-            new Center(
-              child: new FlatButton(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.redAccent,
-                onPressed: () => {
-                  Navigator.pop(context)},
-                child: new Text("もどる"),
-              ),
-            ),
-          ]
+          ),
+        ),
+        new Image.asset('images/logo.png'),
+      ]),
+      );
+  }
+}
+
+class AnimalPage2Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("images/animalpage2_bg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimalPage3Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("images/animalpage3_bg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
   }
