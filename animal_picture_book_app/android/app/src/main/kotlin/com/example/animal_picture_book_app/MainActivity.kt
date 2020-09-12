48c095d9125a1d9f3a2ee0afb9f033de02e01cdd
osmmyk <tavjzr3@gmail.com>
2020-09-12 19:39:27 +0900

    “firebase”

--- animal_picture_book_app/android/app/src/main/kotlin/com/example/animal_picture_book_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.animal_picture_book_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ discovery_picture_book/android/app/src/main/kotlin/osm/myk/discovery_picture_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package osm.myk.discovery_picture_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
